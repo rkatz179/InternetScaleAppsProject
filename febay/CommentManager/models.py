aeef291b474ca816174848bb8681b526d87b1f48
Kevin <kwa6nf@virginia.edu>
2017-09-20 00:28:10 -0400

    created comment model

--- febay/CommentManager/models.py
+++ febay/CommentManager/models.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+from UserManager import models
+from ItemManager import models
 
 # Create your models here.
+
+class Comment(models.Model):
+	user = models.ForeignKey(UserManager.customer)
+	item = models.ForeignKey(ItemManager.Item)
+	message = models.CharField(max_length=200)
+	date_posted = models.DateTimeField(default=timezone.now)
