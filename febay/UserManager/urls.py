23578700b6986df3a458cb675f4da3bb378bf463
Rachel Katz <rak5ea@virginia.edu>
2017-09-20 23:52:56 -0400

    adding services to user

--- febay/UserManager/urls.py
+++ febay/UserManager/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
 	url(r'^api/register', views.register_user, name='registration'),
 	url(r'^api/get/(?P<id>\d*)$', views.get_user, name='get_user'),
-    # url(r'^api/create/$', create),
-    # url(r'^api/update/(?P<id>\d*)$', update),
-    # url(r'^api/delete/(?P<id>\d*)$', delete),
+    url(r'^api/get_users/$', views.get_users, name='get_users'),
+    url(r'^api/login/$', views.login, name='login'),
+    url(r'^api/logout/$', views.logout, name='logout'),
+    url(r'^api/delete_user/$', views.delete_user, name='delete_user'),
+    url(r'^api/update_user_email/$', views.update_user_email, name='update_user_email'),
+
 ]
